2195190e9efc2fbbb2d1ace8b8c08d3e010a91c3
Alexander Saberdine <Alexander@alexanders-macbook-pro.local>
2016-02-24 19:49:17 +0100

    Initial Commit

---
+++ ReallyRandom/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ReallyRandom
+//
+//  Created by Alexander Saberdine on 24/02/16.
+//  Copyright © 2016 Alexander Saberdine. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
