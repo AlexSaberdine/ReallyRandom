45b294a37e446e553f58475732343865d94f5364
Alexander Saberdine <alexander@saberdine.com>
2016-02-24 19:57:21 +0100

    O_o

---
+++ ReallyRandom/Processor.swift
@@ -0,0 +1,9 @@
+//
+//  Processor.swift
+//  ReallyRandom
+//
+//  Created by Alexander Saberdine on 24/02/16.
+//  Copyright © 2016 Alexander Saberdine. All rights reserved.
+//
+
+import Foundation
